4e32061771ef3c968b7cd732e354ab4da2ef5812
Erik Carlsson <info@code301.com>
2015-01-03 14:55:08 +0100

    "Init."

---
+++ lib/Dist/Zilla/Plugin/MapMetro/MakeGraphViz.pm
@@ -0,0 +1,28 @@
+package Dist::Zilla::Plugin::MapMetro::MakeGraphViz;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Zilla::Plugin::MapMetro::MakeGraphViz - Short intro
+
+=head1 SYNOPSIS
+
+  use Dist::Zilla::Plugin::MapMetro::MakeGraphViz;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::MapMetro::MakeGraphViz is ...
+
+=head1 SEE ALSO
+
+=cut
